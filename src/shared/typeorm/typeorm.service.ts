c90b5a5da6dc2a64f7efa8a20e8dfe7e78c3622f
Vladyslav Ievdokymov <52925449+minorsetback@users.noreply.github.com>
2023-04-01 12:19:11 +0300

    add fields

--- src/shared/typeorm/typeorm.service.ts
+++ src/shared/typeorm/typeorm.service.ts
@@ -15,9 +15,8 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       database: this.config.get<string>('DATABASE_NAME'),
       username: this.config.get<string>('DATABASE_USER'),
       password: this.config.get<string>('DATABASE_PASSWORD'),
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
-      migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+      entities: ['dist/**/*.entity{.ts,.js}'], // '**/*.entity{.ts,.js}'
+      migrations: ['dist/migrations/*.{ts,js}'],
       migrationsTableName: 'typeorm_migrations',
       logger: 'file',
       // synchronize: true, // never use TRUE in production!
